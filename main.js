ddb6814e316f1f4e1d38a805f8c3cb936de179fd
minungHan <hmu332233@gmail.com>
2022-01-02 02:09:30 +0900

    feat: 먹었어요 페이지가 아닐 경우 먹었어요 페이지로 이동하고 먹었어요 클릭하도록 변경

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ const P_URL = process.env.P_URL;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(P_URL);
-  await page.screenshot({ path: 'example.png' });
+  
+  console.log('페이지 접속 완료');
 
+  try {
+    await page.click('td.today');
+    console.log('먹었어요 페이지 이동');
+  } catch (err) {
+    console.log('첫 페이지가 캘린더 페이지');
+  }
+  
+  await page.click('#today button.btn[data-value=Y]');
+  console.log('먹었어요 클릭');
   await browser.close();
 })();
